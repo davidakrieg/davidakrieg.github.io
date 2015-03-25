25fd5e541eba48d89b3bd437a8e6f569f137e01e
David Krieg <david.a.krieg@gmail.com>
2015-03-25 05:13:41 -0700

    Base page with random color

---
+++ src/main/scala/please/package.scala
@@ -0,0 +1,9 @@
+package please
+
+import scala.scalajs.js
+/**
+ * Created by kriegd on 3/25/15.
+ */
+package object please extends js.GlobalScope {
+  val Please: PleaseStatic = js.native
+}
---
+++ src/test/scala/please/PleaseTest.scala
@@ -0,0 +1,8 @@
+package please
+
+/**
+ * Created by kriegd on 3/25/15.
+ */
+class PleaseTest {
+
+}
